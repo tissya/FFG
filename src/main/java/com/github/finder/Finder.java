a520b06a85adfe6b9886929de920ae74e9381eae
tissya <tissya@tanakayouyuu-no-MacBook-Air.local>
2015-07-01 14:52:05 +0900

    #8の課題作成
    #8の内容に沿うよう変更を加えた

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -27,6 +27,9 @@ public class Finder {
 		if(args.getType() != null){
             flag &= checkTargetType(file, args.getType());
         }
+		if(args.getSize() != null){
+            flag &= checkTargetSize(file, args.getSize());
+        }
         return flag;
     }
 	
@@ -45,6 +48,26 @@ public class Finder {
         }
         else if(type.equals("h") || type.equals("hidden")){
             return file.isHidden();
+        }
+        return false;
+    }
+	
+	private boolean checkTargetSize(File file, String sizeString){
+        if(file.isFile()){
+            char sign = sizeString.charAt(0);
+            String string = sizeString.substring(1);
+            int size = Integer.parseInt(string);
+
+            switch(sign){
+            case '>':
+                return file.length() > size;
+            case '<':
+                return file.length() < size;
+            case '=':
+                return file.length() == size;
+            default:
+                // ignore
+            }
         }
         return false;
     }
